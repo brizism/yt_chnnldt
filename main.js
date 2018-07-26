38f1bec0a1e98b851e4e0d19b401e53cd9a772a9
brizism <montielbriseida@gmail.com>
2018-07-25 23:07:44 -0400

    load auth2 library and API client library

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
+// Load auth2 library
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
