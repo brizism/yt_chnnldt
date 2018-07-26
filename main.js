fda60d53da916f674e8da8c28b941efee03a4bae
brizism <montielbriseida@gmail.com>
2018-07-26 00:31:53 -0400

    change user channel

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+const defaultChannel = 'brizism';
 
 // Load auth2 library
 function handleClientLoad() {
